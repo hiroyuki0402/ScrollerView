fcfd92475682dc0bf4214eccdf3872ad5be19815
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-12-04 22:12:10 +0900

    first commit

---
+++ ScrollerView/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  ScrollerView
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/12/04.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
